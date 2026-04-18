a82f13b94871610ab0742b574167b4817b41f364
charlgalinato08 <charlgalinato08@gmail.com>
2026-04-18 21:52:16 +0800

    AssemblyInfo.cs

--- GALINATO_IT202WM_ASSIGNMENT2_MIDTERM/Properties/AssemblyInfo.cs
+++ GALINATO_IT202WM_ASSIGNMENT2_MIDTERM/Properties/AssemblyInfo.cs
@@ -2,9 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+
 [assembly: AssemblyTitle("GALINATO_IT202WM_ASSIGNMENT2_MIDTERM")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,20 +12,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("4704b2ac-e81d-482b-ac19-993cb9daac0c")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
